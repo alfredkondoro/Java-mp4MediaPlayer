053319581fd7b52dbc5288114c539db3f364a788
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-06-18 04:33:01 +0300

    version 1.0 - without media controls

---
+++ src/mediaplayer/FXMLDocumentController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediaplayer;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
+
+/**
+ *
+ * @author Redmal
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private String filePath;
+    private MediaPlayer mediaPlayer;
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private MediaView mediaView;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter filter =new FileChooser.ExtensionFilter("Mp4 file please","*.mp4");
+                
+        fileChooser.getExtensionFilters().add(filter);
+        
+        File file = fileChooser.showOpenDialog(null);
+        
+        filePath = file.toURI().toString();
+        
+        if(filePath != null){
+            Media media = new Media (filePath);
+            mediaPlayer = new MediaPlayer(media);
+            mediaView.setMediaPlayer(mediaPlayer);
+            mediaPlayer.play();
+        }
+        
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
